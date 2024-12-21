d435c88a77bdd3d5594a20080eb7353d32a48fce
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2024-12-21 02:25:19 +0100

    Made the active students page dynamic

--- app/Controllers/ActiveStudents.php
+++ app/Controllers/ActiveStudents.php
@@ -3,14 +3,19 @@
 
 namespace App\Controllers;
 
-// use App\Models\RegisteredStudentsModel;
+use App\Models\StudentModel;
 
 class ActiveStudents extends BaseController
 {
     public function active()
     {
-       
-        return view('active_students/active_students');
+        $studentModel = new StudentModel();
+
+        // Fetch active students
+        $data['students'] = $studentModel->where('status', 'Active')->findAll();
+
+        // Load the view and pass data
+        return view('active_students/active_students', $data);
 
     }
 
